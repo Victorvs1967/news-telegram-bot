be7964776f67e0aec5064d66db543408bd8cc982
Victors1967 <victorsmirnov@me.com>
2021-03-22 17:07:06 +0200

    create

---
+++ news-telegram-bot/src/test/java/com/vvs/newstelegrambot/NewsTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvs.newstelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
